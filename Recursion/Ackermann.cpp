d71f904f4ed24b40744673a3d6beefdee75b2e70
Carla de Beer <carla.debeer.uk@gmail.com>
2014-02-19 10:46:33 +0000

    Comments added

--- Recursion/Ackermann.cpp
+++ Recursion/Ackermann.cpp
@@ -9,25 +9,25 @@
 #include <cstring>
 using namespace std;
 
-int ack(int m, int n);
+int ackermann(int m, int n);
 
 int main()
 {    
-    cout <<  ack(1, 2) << endl;	
+    cout <<  ackermann(1, 2) << endl;	
     return 0;
 }
 
-int ack(int m, int n)
+int ackermann(int m, int n)
 {
  if(m == 0)
    return n + 1;
  else if (n == 0 && m > 0)
  {
-    return ack(m-1, 1);
+    return ackermann(m-1, 1);
 
  }
  else
  {
-    return ack((m-1), ack(m, n-1));
+    return ackermann((m-1), ackermann(m, n-1));
  }
 }
